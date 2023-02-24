e1615aa232af38ab345e3de41050e8d576de4212
François Paupier <francois.paupier@gmail.com>
2023-02-24 14:26:40 +0100

    add(example): for piecewise linear function
    #2

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from src.PiecewiseFunctions.PiecewiseConstantFunction import PiecewiseConstantFunction
+from src.PiecewiseFunctions.PiecewiseLinearFunction import PiecewiseLinearFunction
 
 
-def main():
+def example_piecewise_constant_function():
     piecewise_fn = PiecewiseConstantFunction(
         breakpoints=[-100.00, -50, 10, 20, 31.0], values=[30, 20, 10, -10]
     )
     piecewise_fn.draw(x_min=-100, x_max=30, num_points=1000)
 
 
+def example_piecewise_linear_function() -> None:
+    piecewise_fn = PiecewiseLinearFunction(
+        breakpoints=[0, 10, 20, 31.0],
+        slopes=[2, 4, 8],
+        intercepts=[0, 1, 2],
+    )
+    piecewise_fn.draw(x_min=0, x_max=30, num_points=1000)
+
+
 if __name__ == "__main__":
-    main()
+    example_piecewise_linear_function()
