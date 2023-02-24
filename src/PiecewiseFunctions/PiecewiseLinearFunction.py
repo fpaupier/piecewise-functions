36f43e55b7b43e00e5712c0436b2060cefba8d47
François Paupier <francois.paupier@gmail.com>
2023-02-24 14:26:11 +0100

    update(`PiecewiseLinearFunction`): add draw
     utils to visualize function
    
    #2

--- src/PiecewiseFunctions/PiecewiseLinearFunction.py
+++ src/PiecewiseFunctions/PiecewiseLinearFunction.py
@@ -1,5 +1,8 @@
 from typing import List, Tuple, Any
 
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 class PiecewiseLinearFunction:
     """
@@ -176,3 +179,31 @@ class PiecewiseLinearFunction:
                 max_value = value
                 argmax = self.breakpoints[i]
         return max_value, argmax
+
+    def draw(
+        self, x_min: float = -10.0, x_max: float = 10.0, num_points: int = 1000
+    ) -> None:
+        """
+        Generate a plot of the function over the input range.
+
+        Args:
+            x_min: lower bound
+            x_max: higher bound
+            num_points: number of points to sample
+
+        Returns: None - print the function
+
+        Raises: `ValueError` if `x_min` and `x_max` are out of the range or at their boundaries
+        """
+        # Generate a range of x values
+        x_values = np.linspace(x_min, x_max, num_points)
+
+        # Evaluate the function at each x value
+        y_values = [self.evaluate(x) for x in x_values]
+
+        # Plot the function
+        plt.plot(x_values, y_values)
+        plt.xlabel("x")
+        plt.ylabel("f(x)")
+        plt.title("Piecewise Constant Function")
+        plt.show()
