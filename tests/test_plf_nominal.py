0b1ee2e3af3e65c4042c9b17243e01aaaa633e00
François Paupier <francois.paupier@gmail.com>
2023-02-24 14:42:37 +0100

    add(test): nominal case for PiecewiseLinearFunction
    #2

--- tests/test_plf_nominal.py
+++ tests/test_plf_nominal.py
@@ -3,11 +3,15 @@ import math
 
 
 class TestNominalPLF:
-    breakpoints = [-math.inf, 0, math.inf]
-    slopes = [2, 3]
-    intercepts = [3, 12]
+    breakpoints = [-math.inf, 0, 10, 30, 70, math.inf]
+    slopes = [2, 3, 7, -3, 1]
+    intercepts = [3, 12, -2, 3, 3]
     plf = PiecewiseLinearFunction(breakpoints, slopes, intercepts)
 
-    def test_evaluate_neg_nt(self):
+    def test_evaluate_neg_int(self):
         evaluation = self.plf.evaluate(-10)
         assert evaluation == 2 * (-10) + 3
+
+    def test_evaluate_border(self):
+        evaluation = self.plf.evaluate(0)
+        assert evaluation == 3 * (0) + 12
